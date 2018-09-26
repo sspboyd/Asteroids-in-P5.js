befe1eb8091c48b2d9086e54545d6c7ebe5889c8
sspboyd <sspboyd@gmail.com>
2018-09-26 09:33:47 -0400

    Added ship and driving controls. Also added in wrap-around functionality to keep ship on screen.

--- sketch.js
+++ sketch.js
@@ -1,18 +1,57 @@
 function Ship() {
     this.pos = createVector(width / 2, height / 2);
     this.r = 10;
-    this.heading = HALF_PI;
-    this.rotation = 0;
+    this.heading = 0; // units are radians
+    this.rotation = 0; // units are radians
+    this.vel = createVector(1,0);
+    this.isBoosting = false;
 
+    this.boosting = function(b){
+        this.isBoosting = b;
+    }
+
+
+    this.update = function(){
+        if(this.isBoosting){
+            this.boost();
+        }
+        this.pos.add(this.vel);
+        this.vel.mult(0.99);
+        this.edges();
+    }
+
+
+this.boost = function(){
+    var force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
+    this.vel.add(force);
+}
 
     this.render = function() {
-        var tr = this.r;
         stroke(255);
         noFill();
+
         translate(this.pos.x, this.pos.y);
-        rotate(this.heading);
+        rotate(this.heading + PI/2);
+
+        var tr = this.r; // shortcut
         triangle(0, -tr, tr, tr, -tr, tr);
     };
+
+    this.edges = function(){
+        var tp = this.pos;
+        var tr = this.r;
+        if(tp.x > width + tr){
+            tp.x = -tr;
+        }else if (tp.x < -tr) {
+            tp.x = width +tr;
+        }
+        if(tp.y > height + tr){
+            tp.y = -tr;
+        }else if (tp.y < -tr) {
+            tp.y = height +tr;
+        }
+    }
 
     this.setRotation = function(a) {
         this.rotation = a;
@@ -28,18 +67,18 @@ var s1;
 function setup() {
     createCanvas(200, 200);
     s1 = new Ship();
-
 }
 
 function draw() {
     background(47);
     s1.render();
-    // s1.turn(TWO_PI / 6);
-
+    s1.turn();
+s1.update();
 }
 
 function keyReleased() {
     s1.setRotation(0);
+    s1.boosting(false);
 }
 
 function keyPressed() {
@@ -47,5 +86,9 @@ function keyPressed() {
         s1.setRotation(0.1);
     } else if (keyCode == LEFT_ARROW) {
         s1.setRotation(-0.1);
+
+    } else if (keyCode == UP_ARROW) {
+        s1.boosting(true);
     }
+    // add WASD controls too, maybe enable two player mode?
 }
