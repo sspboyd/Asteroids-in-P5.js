65b46424c997f8fbd80bc98cde428b2ef32b6405
sspboyd <sspboyd@gmail.com>
2018-09-30 00:24:40 -0400

    Added three game states: pregame, activeGame, postGame. Created new object to hold vars for these states and initialize, play and reset a game. Changed the canvas size to the width of the window. Moved the scoreboard object literal into its own file. Updated the keyboard ui to manage the different inputs for different game states.

--- sketch.js
+++ sketch.js
@@ -1,136 +1,134 @@
-var s1;
-var asteroids = [];
-var lasers = [];
-const PHI = 1.6180339;
+    var s1;
+    var asteroids = [];
+    var lasers = [];
+    const PHI = 1.6180339;
 
-
-function setup() {
-    createCanvas(700, 600);
-    s1 = new Ship();
-
-    for (var i = 0; i < 10; i++) {
-        asteroids.push(new Asteroid());
-    }
-
-}
-
-function draw() {
-    background(47);
-    s1.turn();
-    s1.update();
-    scoreboard.update();
-    s1.render();
-    s1.isHit = false;
-
-
-    asteroids.forEach(function(ast) {
-        ast.update();
-        if (s1.isCollision(ast)) {
-            s1.isHit = true;
-            scoreboard.incrmt("collisionCount");
-        }
-    });
-
-    // for (var i in asteroids) {
-    //     asteroids[i].update();
-    //     if (s1.isCollision(asteroids[i])) {
-    //         s1.isHit = true;
-    //         scoreboard.incrmt("collisionCount");
-    //     }
-    // }
-
-    // for (var i = 0; i < asteroids.length; i++) {
-    //     asteroids[i].update();
-    //     if (s1.isCollision(asteroids[i])) {
-    //         s1.isHit = true;
-    //         scoreboard.incrmt("collisionCount");
-    //     }
-    // }
-
-    for (var i = lasers.length - 1; i >= 0; i--) {
-        lasers[i].update();
-        if (lasers[i].isOutOfBounds()) {
-            lasers.splice(i, 1);
-            break;
-        }
-        for (var j = asteroids.length - 1; j >= 0; j--) {
-            if (lasers[i].hits(asteroids[j])) {
-                scoreboard.incrmt("hitCount");
-
-                var newAsteroids = asteroids[j].breakup();
-                asteroids.splice(j, 1);
-                lasers.splice(i, 1);
-                asteroids = asteroids.concat(newAsteroids);
-                break;
+    let gameState = {
+        highScore: 0,
+        // pregame: true, //ready for new game to begin
+        // activeGame: false, //
+        // postGame: false,
+        initGame: function() {
+            this.pregame = true;
+            this.activeGame = false;
+            this.postgame = false;
+            scoreboard.initScore();
+            s1 = new Ship();
+            for (var i = 0; i < 18; i++) {
+                asteroids.push(new Asteroid());
             }
         }
+    };
+
+
+    function setup() {
+        createCanvas(windowWidth - 20, windowHeight - 20);
+        gameState.initGame();
     }
-}
 
+    function draw() {
+        background(4, 7, 18);
 
-let scoreboard = {
-    score: 0,
-    collisionCount: 0,
-    laserCount: 0,
-    hitCount: 0,
-    distance: 0,
-    maxScore: 0,
-    update: function() {
-        this.score = this.calcScore();
-        this.display();
-        if (this.score > this.maxScore) {
-            this.maxScore = this.score;
+        if (gameState.pregame) {
+            asteroids.forEach(function(ast) {
+                ast.update();
+            });
+            scoreboard.update();
+
+            textSize(72);
+            fill(255, 199);
+            text("Asteroids", width / pow(PHI, 4), height / PHI);
+            textSize(24);
+            fill(123, 199);
+            text("left, right and up arrows to move, Spacebar to shoot\nyou can't die, just try to get points\npress 'enter' to begin.", width / pow(PHI, 4), height / PHI + 72);
+        } else if (gameState.activeGame) {
+            if (asteroids.length < 1) {
+                gameState.activeGame = false;
+                gameState.postGame = true;
+            }
+            scoreboard.update();
+            s1.turn();
+            s1.update();
+            s1.render();
+            s1.isHit = false;
+
+            asteroids.forEach(function(ast) {
+                ast.update();
+                if (s1.isCollision(ast)) {
+                    s1.isHit = true;
+                    scoreboard.incrmt("collisionCount");
+                }
+            });
+
+            for (var i = lasers.length - 1; i >= 0; i--) {
+                lasers[i].update();
+                if (lasers[i].isOutOfBounds()) {
+                    lasers.splice(i, 1);
+                    break;
+                }
+                for (var j = asteroids.length - 1; j >= 0; j--) {
+                    if (lasers[i].hits(asteroids[j])) {
+                        scoreboard.incrmt("hitCount");
+
+                        var newAsteroids = asteroids[j].breakup();
+                        asteroids.splice(j, 1);
+                        lasers.splice(i, 1);
+                        asteroids = asteroids.concat(newAsteroids);
+                        break;
+                    }
+                }
+            }
+        } else if (gameState.postGame) {
+            fill(255, 123, 199, 76);
+            textSize(72);
+            text("Game Over", width / pow(PHI, 4), height / PHI);
+            textSize(24);
+            text("press 'enter' to restart the game", width / pow(PHI, 4), height / PHI + 72);
+            scoreboard.update();
+
         }
-    },
-
-    incrmt: function(kval) {
-        if (kval in this) {
-            this[kval.toString()] += 1;
-        }
-        // console.log(kval.toString(), this[kval.toString()]);
-    },
-    display: function() {
-        push(); // for style state
-        fill(255, 199, 123);
-        textSize(18);
-        text("Collisions = " + this.collisionCount, 10, height - 11);
-        text("Shots = " + this.laserCount, 150, height - 11);
-        text("Hits = " + this.hitCount, 250, height - 11);
-        fill(123, 199, 255);
-        text("Score = " + this.score, width - 150, height - 11);
-        text("Max Score = " + this.maxScore, width - 300, height - 11);
-    },
-    calcScore: function() {
-        let hitScore = this.hitCount * 50;
-        let laserScore = -this.laserCount * 2;
-        let collisionScore = this.collisionCount * -4;
-        return hitScore + laserScore + collisionScore;
     }
-}
 
-function keyReleased() {
-    s1.setRotation(0);
-    s1.boosting(false);
-}
-
-function keyPressed() {
-    if (key == ' ') { // only for one player. will need another keypressed key for second player
-        lasers.push(new Laser(s1));
-        scoreboard.incrmt("laserCount");
-
-    }
-    if (keyCode == RIGHT_ARROW) {
-        s1.setRotation(0.1);
-    } else if (keyCode == LEFT_ARROW) {
-        s1.setRotation(-0.1);
-
-    } else if (keyCode == UP_ARROW) {
-        s1.boosting(true);
-        // s1.boosting(1);
-
-    } else if (keyCode == DOWN_ARROW) {
+    function keyReleased() {
+        s1.setRotation(0);
         s1.boosting(false);
-        // s1.boosting(-1);
     }
-    // add WASD controls too, maybe enable two player mode?
-}
+
+    function keyPressed() {
+        if (keyCode === 13) { // only for one player. will need another keypressed key for second player
+            if (gameState.pregame) {
+                gameState.pregame = false;
+                gameState.activeGame = true;
+                gameState.postGame = false;
+
+            } else if (gameState.postGame) {
+                gameState.pregame = true;
+                gameState.activeGame = false;
+                gameState.postGame = false;
+
+                gameState.initGame();
+            }
+        }
+
+        if (gameState.activeGame) {
+            if (key == ' ') { // only for one player. will need another keypressed key for second player
+                lasers.push(new Laser(s1));
+                scoreboard.incrmt("laserCount");
+            }
+
+            if (keyCode == RIGHT_ARROW) {
+                s1.setRotation(0.11);
+            } else if (keyCode == LEFT_ARROW) {
+                s1.setRotation(-0.11);
+
+            } else if (keyCode == UP_ARROW) {
+                s1.boosting(true);
+                // s1.boosting(1);
+
+            } else if (keyCode == DOWN_ARROW) {
+                s1.boosting(false);
+                // s1.boosting(-1);
+            }
+            // add WASD controls too, maybe enable two player mode?
+        }
+    }
